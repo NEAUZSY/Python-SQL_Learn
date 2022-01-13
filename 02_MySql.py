fe94959e37fb5983f712d65fe8727f9eddd791b6
NEAUZSY <62225745+NEAUZSY@users.noreply.github.com>
2022-01-13 11:24:08 +0800

    添加数据到数据库

--- 02_MySql.py
+++ 02_MySql.py
@@ -7,7 +7,7 @@ def main():
                            db='school', charset='utf8')
     try:
         with conn.cursor() as cursor:
-            result = cursor.execute('SELECT stuname, colid FROM tb_student;')
+            result = cursor.execute('insert tb_college values( 5, "水利学院","www.baidu.com" );')
             print(result)
         conn.commit()
     except pymysql.MySQLError as error:
